3093d98133efa016fef3f4b7a01028a6e9c60eaa
Ruslan Gunawardana <rusan.rus@gmail.com>
2016-01-21 00:00:42 +0200

    Mirror random: roles are now reasinged in case if team member doesn't have any hero of assigned role. Exceptions are thrown on uneven or too big teams.

--- src/hots/core.clj
+++ src/hots/core.clj
@@ -5,8 +5,9 @@
             [clojure.data.json :as json]
             [clojure.java.io :as io]))
 
-(defn validate-heroes-db []
-  (let [heroes-txt (slurp (-> "heroes.json" io/resource io/file))
+(defn validate-heroes-db
+  []
+  (let [heroes-txt (-> "heroes.json" io/resource io/file slurp)
         heroes-json (json/read-str heroes-txt :key-fn keyword)
         names (set (json-path/at-path "$[*].name" heroes-json))]
     (every? (fn [heroes]
@@ -14,46 +15,60 @@
                       heroes))
             (vals db/heroes-map))))
 
-(defn get-heroes-for [owner free-pick]
+(defn get-heroes-for
+  [owner free-pick]
   (vec (union (owner db/heroes-map)
               (if free-pick db/week-heroes))))
 
-(defn random-hero [owner free-pick & {:keys [role type]}]
-  (let [heroes-txt (slurp (-> "heroes.json" io/resource io/file))
-        heroes-json (json/read-str heroes-txt :key-fn keyword)
-        allowed-heroes (set (map :name
-                                 (filter (fn [e] (and (or (nil? role) (= role (:role e)))
-                                                      (or (nil? type) (= type (:type e)))))
-                                         heroes-json)))]
-    (rand-nth (filter (fn [h] (contains? allowed-heroes h))
-                      (get-heroes-for owner free-pick)))))
+(defn random-hero
+  [owner free-pick & {:keys [role type]}]
+  (let [owner-allowed-heroes
+        (if (or (some? role) (some? type))
+          (let [all-heroes-json (-> "heroes.json" io/resource io/file slurp)
+                all-heroes (json/read-str all-heroes-json :key-fn keyword)
+                by-role-and-type (fn [e] (and (or (nil? role) (= role (:role e)))
+                                              (or (nil? type) (= type (:type e)))))
+                allowed-heroes (set (map :name
+                                         (filter by-role-and-type all-heroes)))]
+            (filter (fn [h] (contains? allowed-heroes h))
+                    (get-heroes-for owner free-pick)))
+          (get-heroes-for owner free-pick))]
+    (if (not-empty owner-allowed-heroes)
+      (rand-nth owner-allowed-heroes)
+      nil)))
 
+(defn random-team
+  [owners free-pick & {:keys [set-up]}]
+  (let [heroes (map (fn [o s] (random-hero o free-pick :role (:role s) :type (:type s)))
+                    owners set-up)]
+    (if (every? some? heroes)
+      (if (apply distinct? heroes)
+        (zipmap owners heroes)
+        (random-team owners free-pick :set-up set-up))
+      (random-team (shuffle owners) free-pick :set-up set-up))))
 
-(defn random-team [owners free-pick & {:keys [set-up]}]
-  (let [heroes (if (nil? set-up)
-                 (map (fn [o] (rand-nth (get-heroes-for o free-pick))) owners)
-                 (map (fn [o s] (random-hero o free-pick (:role s) (:type s))) owners set-up))]
-    (if (apply distinct? heroes)
-      (zipmap owners heroes)
-      (random-team owners free-pick :set-up set-up))))
-
-(defn plain-random-teams [members & {:keys [free-pick]}]
+(defn plain-random-teams
   "Plain random teams"
-  (if (and (<= (count members) 10) (= (mod (count members) 2) 0))
-    [(random-team (subvec members 0 (/ (count members) 2)) free-pick)
-     (random-team (subvec members (/ (count members) 2)) free-pick)]))
+  [members & {:keys [free-pick]}]
+  (if (and (<= (count members) 10) (even? (count members)))
+    (let [team-size (/ (count members) 2)]
+      [(random-team (subvec members 0 team-size) free-pick)
+       (random-team (subvec members team-size) free-pick)])
+    (throw (IllegalArgumentException. "There should be 2, 4, 6, 8 or 10 members."))))
 
-(defn mirror-random-teams [members & {:keys [free-pick]}]
+(defn mirror-random-teams
   "Random teams with mirror set-ups."
-  (if (and (<= (count members) 10) (= (mod (count members) 2) 0))
+  [members & {:keys [free-pick]}]
+  (if (and (<= (count members) 10) (even? (count members)))
     (let [members (shuffle members)
           team-size (/ (count members) 2)
           set-up (rand-nth (filter (fn [e] (= team-size (count e))) db/set-ups))]
       [(random-team (subvec members 0 team-size) free-pick :set-up set-up)
-       (random-team (subvec members team-size) free-pick :set-up set-up)])))
+       (random-team (subvec members team-size) free-pick :set-up set-up)])
+    (throw (IllegalArgumentException. "There should be 2, 4, 6, 8 or 10 members."))))
 
 (defn -main
   [& args]
-  (let [teams (plain-random-teams [:Anton :Konstantine :Ruslan :Vlad :Shiron :Bratus] :free-pick true)]
+  (let [teams (mirror-random-teams [:Anton :Konstantine :Ruslan :Vlad :Shiron :Bratus] :free-pick true)]
     (println (str "Team 1: " (first teams)))
     (println (str "Team 2: " (second teams)))))
