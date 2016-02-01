53c1b4639f279d1c09b16824a0beaf29960a19a0
Ruslan Gunawardana <rusan.rus@gmail.com>
2016-02-01 23:39:28 +0200

    Description changed

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hots "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library designed to randomize hero picks for HOTS custom games"
+  :url "http://us.battle.net/heroes/en/"
   :license {:name "GNU General Public License v3.0"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
