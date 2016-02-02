3dace69ec4c459c67f7f1268e15db857867001b0
Ruslan Gunawardana <rusan.rus@gmail.com>
2016-02-02 03:37:05 +0200

    Project file reafactiong

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Library designed to randomize hero picks for HOTS custom games"
   :url "http://us.battle.net/heroes/en/"
   :license {:name "GNU General Public License v3.0"
-            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
+            :url  "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [json-path "0.2.0"]
-				 [org.clojure/math.combinatorics "0.1.1"]
-				 [org.clojure/tools.trace "0.7.5"]]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [org.clojure/tools.trace "0.7.5"]]
   :main ^:skip-aot hots.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
