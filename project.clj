3336fcf4977b07fc09aa846ad26720fd9e96e574
Ruslan Gunawardana <rusan.rus@gmail.com>
2016-02-07 10:13:55 +0200

    Project URL deleted

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject hots "0.1.0-SNAPSHOT"
   :description "Library designed to randomize hero picks for HOTS custom games"
-  :url "http://us.battle.net/heroes/en/"
   :license {:name "GNU General Public License v3.0"
             :url  "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
