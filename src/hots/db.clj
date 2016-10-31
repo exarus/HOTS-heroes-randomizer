ae4a324643b7e47a68abcd67210540c8895a364c
Vlad <valsid@users.noreply.github.com>
2016-10-31 22:49:19 +0200

    Update db.clj

--- src/hots/db.clj
+++ src/hots/db.clj
@@ -50,7 +50,7 @@
    :Stepanov    #{"Anub'arak" "Arthas" "Azmodan" "Cho" "Diablo" "E.T.C." "Gall" "Gazlowe" "Illidan" "Johanna"
                   "Kharazim" "Leoric" "Li Li" "Lunara" "Malfurion" "Muradin" "Raynor" "Rehgar" "Sonya" "Tassadar"
                   "Tychus" "Tyrael" "Valla" "Xul"}
-   :Vlad        #{"Abathur" "Anub'arak" "Artanis" "Brightwing" "Butcher" "Chen" "Cho" "Chromie" "E.T.C." "Falstad"
+   :Vlad        #{"Abathur" "Anub'arak" "Artanis" "Azmodan" "Brightwing" "Butcher" "Chen" "Cho" "Chromie" "E.T.C." "Falstad"
                   "Gall" "Gazlowe" "Greymane" "Illidan" "Jaina" "Johanna" "Kael'thas" "Kerrigan" "Kharazim" "Li Li" "Li-Ming"
                   "Lunara" "Malfurion" "Muradin" "Murky" "Nazeebo" "Raynor" "Sylvanas" "Sonya" "Stitches" "Tassadar" 
                   "Thrall" "Tracer" "Tychus" "Tyrael" "Tyrande" "Uther" "Valla" "Xul" "Zagara"}})
